88e5bdc23a12eb76f293c243f72f08209eb7fae9
julien <mp.dev.mg@gmail.com>
2024-11-04 18:31:26 +0100

    Gestion des états d'affichage de la modale d'inscription à l'événement
    Ajout des fonctionnalités pour ouvrir et fermer la modale d'inscription via des gestionnaires d'événements de clic, en assurant un affichage dynamique en fonction des actions de l'utilisateur.

--- starterOnly/javascript/index.js
+++ starterOnly/javascript/index.js
@@ -1,12 +1,14 @@
 import { topNavEventLink } from "./topNavEventLink.js";
 import { topNavMenuToggle } from "./topNavMenuToggle.js";
+import { modalRegisterDisplay } from "./modalRegisterDisplay.js";
 
 topNavEventLink();
 topNavMenuToggle();
+modalRegisterDisplay();
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
+const modalbg = document.querySelector(".modal-register");
+const modalBtn = document.querySelectorAll(".button-modal");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
